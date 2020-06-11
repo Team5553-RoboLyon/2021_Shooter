fa5a7f3eb02361fddc4eb5d158052a21492d598c
Nathan <nathan.mutin@gmail.com>
2020-06-11 17:36:50 +0200

    initial commit

---
+++ src/main/cpp/Robot.cpp
@@ -0,0 +1,73 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "Robot.h"
+
+#include <iostream>
+#include <frc/shuffleboard/Shuffleboard.h>
+
+void Robot::RobotInit()
+{
+    m_RightMotor.RestoreFactoryDefaults();
+    m_LeftMotor.RestoreFactoryDefaults();
+
+    m_RightMotor.SetOpenLoopRampRate(0.6);
+    m_LeftMotor.SetOpenLoopRampRate(0.6);
+
+    //m_RightMotor.EnableVoltageCompensation(11.5);
+    //m_LeftMotor.EnableVoltageCompensation(11.5);
+
+    m_LeftMotor.SetInverted(true);
+
+    m_ShooterEnabled = false;
+
+    m_PowerEntry = frc::Shuffleboard::GetTab("Shooter").Add("Power", 0.0).WithWidget(frc::BuiltInWidgets::kNumberSlider).GetEntry();
+}
+
+void Robot::AutonomousInit() {}
+void Robot::AutonomousPeriodic() {}
+
+void Robot::TeleopInit() {}
+void Robot::TeleopPeriodic()
+{
+    if (m_DriverController.GetAButtonPressed())
+    {
+        m_ShooterEnabled = !m_ShooterEnabled;
+        if (m_ShooterEnabled)
+        {
+            m_LogFile = new CSVLogFile("/home/lvuser/logs/log", "Power", "Right", "Left");
+        }
+        else
+        {
+            delete m_LogFile;
+        }
+    }
+
+    if (m_ShooterEnabled)
+    {
+        double power = m_PowerEntry.GetDouble(0.0);
+        m_RightMotor.Set(power);
+        m_LeftMotor.Set(power);
+        std::cout << power << "  " << m_RightEncoder.GetVelocity() << "  " << m_LeftEncoder.GetVelocity() << std::endl;
+        m_LogFile->Log(power, m_RightEncoder.GetVelocity(), m_LeftEncoder.GetVelocity());
+    }
+    else
+    {
+        m_RightMotor.StopMotor();
+        m_LeftMotor.StopMotor();
+    }
+}
+
+void Robot::TestInit() {}
+void Robot::TestPeriodic() {}
+
+#ifndef RUNNING_FRC_TESTS
+int main()
+{
+    return frc::StartRobot<Robot>();
+}
+#endif
---
+++ src/main/include/Robot.h
@@ -0,0 +1,42 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#pragma once
+
+#include <frc/TimedRobot.h>
+#include <frc/XboxController.h>
+#include <networktables/NetworkTableEntry.h>
+#include <rev/CANSparkMax.h>
+
+#include "lib/CSVLogFile.h"
+
+class Robot : public frc::TimedRobot
+{
+public:
+  void RobotInit() override;
+
+  void AutonomousInit() override;
+  void AutonomousPeriodic() override;
+
+  void TeleopInit() override;
+  void TeleopPeriodic() override;
+
+  void TestInit() override;
+  void TestPeriodic() override;
+
+private:
+  frc::XboxController m_DriverController{0};
+  nt::NetworkTableEntry m_PowerEntry;
+  CSVLogFile *m_LogFile;
+
+  rev::CANSparkMax m_RightMotor{1, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANSparkMax m_LeftMotor{2, rev::CANSparkMax::MotorType::kBrushless};
+  rev::CANEncoder m_RightEncoder{m_RightMotor};
+  rev::CANEncoder m_LeftEncoder{m_LeftMotor};
+
+  bool m_ShooterEnabled;
+};
